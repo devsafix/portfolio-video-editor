037dd40f1e361261e3015eaae92ce24193cc447d
devsafix <devsafix@gmail.com>
2024-12-12 02:03:40 +0600

    Banner Done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,73 @@
+import { Typewriter } from "react-simple-typewriter";
+import PrimaryButton from "../ui/PrimaryButton";
+
+const Banner = () => {
+    return (
+        <div className="bg-gray-950 lg:pt-16 pt-6 relative">
+            <div className="container py-28 text-white ">
+                {/* Left Section */}
+                <div className="flex lg:flex-row flex-col items-center justify-between">
+                    <div className="max-w-2xl">
+                        <h1 className="text-themeColor text-3xl italic mb-3 bilbo-text">Creative Thinking</h1>
+                        <h1 className="leading-tight mb-6">
+                            Strategic Thinking, Tangible Results for you!
+                        </h1>
+                        <h3 className="mb-7">
+                            We Provide <span className="text-themeColor lg:text-[40px] text-[25px]">
+                                <Typewriter
+                                    words={['Video Editing', 'Motion Graphics', 'Cinematography']}
+                                    loop={5}
+                                    cursor
+                                    cursorStyle='|'
+                                    typeSpeed={70}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+                                />
+                            </span>
+                        </h3>
+                        <p className="text-gray-400 mb-7">
+                            We are a leading digital services provider, offering solutions for online
+                            business success. Our expert team delivers customized Video Editing,
+                            Motion Graphics, and IT support to achieve results in the evolving
+                            digital landscape. Count on us to guide you.
+                        </p>
+                        <PrimaryButton title={"Explore Now"} />
+                    </div>
+                    {/* Right Section */}
+                    <div className="relative mt-12 lg:mt-0">
+                        <div className="rounded-full bg-gradient-to-r from-teal-400 to-green-600 p-1 animate-up-down">
+
+                            <img
+                                src="https://lambup.com/wp-content/uploads/2024/10/Home-Page-Image-2.png"
+                                alt="Editing Screen"
+                                className="rounded-full w-[350px] h-[550px] lg:w-[370px] lg:h-[550px] inset-0 object-cover bg-cover bg-center"
+                            />
+                        </div>
+                        <div className="absolute bottom-7 lg:-left-40  rounded-full bg-gray-700 p-2 animate-scale">
+
+                            <img
+                                src="https://lambup.com/wp-content/uploads/2024/10/Home-Page-Image-1.png"
+                                alt="Editing Screen"
+                                className="rounded-full w-[200px] h-[300px] lg:w-[250px] lg:h-[350px] inset-0 object-cover bg-cover bg-center"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="absolute lg:block hidden right-40 top-32 animate-scale">
+                <img src="https://lambup.com/wp-content/uploads/2024/10/hero-shape-6.webp" alt="" />
+            </div>
+            <div className="absolute lg:block hidden left-10 top-32 animate-up-down size-40 opacity-30">
+                <img src="https://lambup.com/wp-content/uploads/2024/10/shape-35.png" alt="" />
+            </div>
+            <div className="absolute lg:block hidden right-[700px] top-32 animate-up-down size-40 opacity-30">
+                <img src="https://lambup.com/wp-content/uploads/2024/10/hero-shape-3.webp" alt="" />
+            </div>
+            <div className="absolute lg:block hidden right-[650px] bottom-20 animate-scale size-40">
+                <img src="https://lambup.com/wp-content/uploads/2024/10/hero-shape-6.webp" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,62 +1,117 @@
-import { useEffect } from "react";
-import { useRef } from "react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PrimaryButton from "../../ui/PrimaryButton";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
-
+    const [isScrolled, setIsScrolled] = useState(false);
     const [dropDownState, setDropDownState] = useState(false);
     const dropDownMenuRef = useRef();
 
     useEffect(() => {
+        // Handle closing dropdown menu when clicking outside
         const closeDropDown = (e) => {
             if (!dropDownMenuRef?.current?.contains(e?.target)) {
                 setDropDownState(false);
             }
         };
+        document.addEventListener("mousedown", closeDropDown);
 
-        document.addEventListener('mousedown', closeDropDown);
+        // Handle navbar background on scroll
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
 
         return () => {
-            document.removeEventListener('mousedown', closeDropDown);
+            document.removeEventListener("mousedown", closeDropDown);
+            window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
     return (
-        <div className="bg-[#393E46]">
-            <nav className="container flex items-center justify-between px-4 py-2 text-white mb-24">
-                <div className="cursor-pointer rounded-2xl px-3 py-2 text-xl font-semibold text-white roboto">
-                    <h3>Portfolio</h3>
+        <div
+            className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${isScrolled ? "bg-black shadow-lg" : "bg-transparent"
+                }`}
+        >
+            <nav className="container flex items-center justify-between py-3 text-white">
+                <div className="cursor-pointer text-xl font-semibold text-white roboto">
+                    {/* <h3 className="font-normal tracking-wider">Lambup</h3> */}
+                    <img 
+                    className="lg:w-56 w-40"
+                    src="https://lambup.com/wp-content/uploads/2024/10/LUMBUP-1-01-01-e1729165527561-1024x326.png" alt="" />
                 </div>
                 <ul className="hidden items-center justify-between gap-10 md:flex">
                     <li className="group flex font-medium cursor-pointer flex-col">
-                        <Link>Home</Link><span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-themeColor transition-all duration-300 group-hover:w-full"></span>
+                        <Link>Home</Link>
+                        <span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-themeColor transition-all duration-300 group-hover:w-full"></span>
                     </li>
                     <li className="group flex font-medium cursor-pointer flex-col">
-                        <Link>Portfolio</Link><span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-themeColor transition-all duration-300 group-hover:w-full"></span>
+                        <Link>About us</Link>
+                        <span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-themeColor transition-all duration-300 group-hover:w-full"></span>
+                    </li>
+                    <li className="group flex font-medium cursor-pointer flex-col">
+                        <Link>Service</Link>
+                        <span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-themeColor transition-all duration-300 group-hover:w-full"></span>
+                    </li>
+                    <li className="group flex font-medium cursor-pointer flex-col">
+                        <Link>Portfolio</Link>
+                        <span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-themeColor transition-all duration-300 group-hover:w-full"></span>
+                    </li>
+                    <li className="group flex font-medium cursor-pointer flex-col">
+                        <Link>Contact</Link>
+                        <span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-themeColor transition-all duration-300 group-hover:w-full"></span>
                     </li>
                 </ul>
                 <div>
                     <PrimaryButton title={"Let's Talk Us"} />
                 </div>
-                <div ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="relative flex transition-transform md:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="cursor-pointer" > <line x1="4" x2="20" y1="12" y2="12" /> <line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /> </svg>
+                <div
+                    ref={dropDownMenuRef}
+                    onClick={() => setDropDownState(!dropDownState)}
+                    className="relative flex transition-transform md:hidden"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="cursor-pointer"
+                    >
+                        <line x1="4" x2="20" y1="12" y2="12" />
+                        <line x1="4" x2="20" y1="6" y2="6" />
+                        <line x1="4" x2="20" y1="18" y2="18" />
+                    </svg>
                     {dropDownState && (
-                        <ul className=" z-10  gap-2  bg-[#393E46]  absolute right-0 top-12 flex w-[200px] flex-col  rounded-lg   text-base ">
-                            <li className="cursor-pointer  px-6 py-2 text-white rounded-t-lg hover:bg-sky-600 ">
+                        <ul className="z-10 gap-2 bg-[#393E46] absolute right-0 top-12 flex w-[200px] flex-col rounded-lg text-base">
+                            <li className="cursor-pointer px-6 py-2 text-white rounded-t-lg hover:bg-sky-600">
                                 <Link>Home</Link>
                             </li>
-                            <li className="cursor-pointer  px-6 py-2 text-white hover:bg-sky-600 ">
+                            <li className="cursor-pointer px-6 py-2 text-white hover:bg-sky-600">
+                                <Link>About Us</Link>
+                            </li>
+                            <li className="cursor-pointer px-6 py-2 text-white hover:bg-sky-600">
+                                <Link>Service</Link>
+                            </li>
+                            <li className="cursor-pointer px-6 py-2 text-white hover:bg-sky-600">
                                 <Link>Portfolio</Link>
+                            </li>
+                            <li className="cursor-pointer px-6 py-2 text-white hover:bg-sky-600">
+                                <Link>Contact</Link>
                             </li>
                         </ul>
                     )}
                 </div>
             </nav>
         </div>
-
     );
 };
 
