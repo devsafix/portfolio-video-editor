f88c83d01089b235a95e6d51eef7405189250cd2
devsafix <devsafix@gmail.com>
2024-12-12 17:20:46 +0600

    Deploy Home page on vercel

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -37,11 +37,11 @@ const Navbar = () => {
             className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${isScrolled ? "bg-black shadow-lg" : "bg-transparent"
                 }`}
         >
-            <nav className="container flex items-center justify-between py-3 text-white">
+            <nav className="container flex items-center justify-between py-4 text-white">
                 <div>
-                    <img 
-                    className="lg:w-56 w-40"
-                    src="https://lambup.com/wp-content/uploads/2024/10/LUMBUP-1-01-01-e1729165527561-1024x326.png" alt="" />
+                    <img
+                        className="lg:w-56 w-40"
+                        src="https://lambup.com/wp-content/uploads/2024/10/LUMBUP-1-01-01-e1729165527561-1024x326.png" alt="" />
                 </div>
                 <ul className="hidden items-center justify-between gap-10 md:flex">
                     <li className="group flex font-medium cursor-pointer flex-col">
@@ -90,7 +90,11 @@ const Navbar = () => {
                         <line x1="4" x2="20" y1="18" y2="18" />
                     </svg>
                     {dropDownState && (
-                        <ul className="z-10 gap-2 bg-[#393E46] absolute right-0 top-14 flex w-[200px] flex-col rounded-lg text-base">
+                        <ul
+                            data-aos="fade-left"
+                            data-aos-delay="50"
+                            data-aos-duration="700"
+                            className="z-10 gap-2 bg-gradient-to-b from-teal-600 to-teal-400 absolute right-0 top-14 flex w-[200px] flex-col rounded-lg text-base">
                             <li className="cursor-pointer px-6 py-2 text-white rounded-t-lg hover:bg-sky-600">
                                 <Link>Home</Link>
                             </li>
